437922b24d3f209fa5e7faf43d64c62d401b5343
MikaGawin <mikasgawin@gmail.com>
2025-09-23 13:05:50 +0100

    Adds command line command for updating data

--- src/WarhammerStats.Cli/Program.cs
+++ src/WarhammerStats.Cli/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.CommandLine;
+using WarhammerStats.Core;
 
 var rootCommand = new RootCommand("Warhammer Stats CLI");
+
+//for testing if cli is working
 
 var helloCommand = new Command("hello", "Say hello");
 var nameArgument = new Argument<string>("name");
@@ -11,6 +14,28 @@ helloCommand.SetHandler((string name) =>
     Console.WriteLine($"Hello, {name}!");
 }, nameArgument);
 
+//update units command
+
+var updateUnitsCommand = new Command("update-units", "Extract all units from munitorum field manual(pdf) and update database");
+var pdfPathArgument = new Argument<string>("pdfPath");
+updateUnitsCommand.Add(pdfPathArgument);
+
+updateUnitsCommand.SetHandler((string pdfPath) =>
+{
+    var parser = new PdfParser();
+    var units = parser.ExtractUnits(pdfPath);
+
+    Console.WriteLine("Units found in PDF:");
+    foreach(var unit in units)
+    {
+        Console.WriteLine($"- {unit}");
+    }
+
+}, pdfPathArgument);
+
+
 rootCommand.Add(helloCommand);
+rootCommand.Add(updateUnitsCommand);
 
 return await rootCommand.InvokeAsync(args);
+
---
+++ src/WarhammerStats.Core/PdfParser.cs
@@ -0,0 +1,15 @@
+namespace WarhammerStats.Core;
+
+public class PdfParser
+{
+    public List<string> ExtractUnits(string pdfPath)
+    {
+        //return placeholders
+        return new List<string>
+        {
+            "intercessors",
+            "gladiator reaper",
+            "captain"
+        };
+    }
+}
