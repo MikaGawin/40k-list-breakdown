7ae3cb9d34c1b028d319cacdfbb4c1e6b720969f
MikaGawin <mikasgawin@gmail.com>
2025-10-03 12:14:30 +0100

    adds a command that reads the points document and extracts all lines

--- src/WarhammerStats.Core/PdfParser.cs
+++ src/WarhammerStats.Core/PdfParser.cs
@@ -1,15 +1,32 @@
+using System.Collections.Generic;
+using System.Linq; // Needed for Select
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
 namespace WarhammerStats.Core;
 
 public class PdfParser
 {
     public List<string> ExtractUnits(string pdfPath)
     {
-        //return placeholders
-        return new List<string>
+        var lines = new List<string>();
+
+        using (var document = PdfDocument.Open(pdfPath))
         {
-            "intercessors",
-            "gladiator reaper",
-            "captain"
-        };
+            foreach (var page in document.GetPages())
+            {
+                // PdfPig gives raw text; may not be "line broken" nicely
+                var text = page.Text ?? string.Empty;
+
+                // Split on both \n and \r\n, remove empty entries, trim whitespace
+                var splitLines = text
+                    .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(l => l.Trim());
+
+                lines.AddRange(splitLines);
+            }
+        }
+
+        return lines;
     }
 }
